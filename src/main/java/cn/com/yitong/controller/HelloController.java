0d41f58720a4408803d14f2dfd9b8bd889ccfac0
zhangliugen <13063419587@163.com>
2019-08-06 16:43:55 +0800

    springboot demo for git

--- src/main/java/cn/com/yitong/controller/HelloController.java
+++ src/main/java/cn/com/yitong/controller/HelloController.java
@@ -14,10 +14,10 @@ public class HelloController {
 	@RequestMapping("/hello.do")
 	public ResponseResult hello() {
 		User u = new User();
-		u.setName("张三");
+		u.setName("张留根");
 		u.setAge("18");
 		u.setBirthdate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
-		u.setDesc("张三的信息");
+		u.setDesc("张留根的信息");
 		return ResponseResult.ok(u);
 	}
 }
